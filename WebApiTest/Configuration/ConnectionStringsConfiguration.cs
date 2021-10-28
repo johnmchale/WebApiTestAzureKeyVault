fa3369bd26a74a74a4118faa8b1706755c045630
John McHale <johnmchale@utilita.co.uk>
2021-10-28 18:07:25 +0100

    Add project files.

---
+++ WebApiTest/Configuration/ConnectionStringsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Configuration
+{
+    public class ConnectionStringsConfiguration
+    {
+        public const string Name = "ConnectionStrings";
+        public string SqlConnectionOracle { get; set; } 
+    }
+ }
---
+++ WebApiTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebApiTest.Configuration;
+
+namespace WebApiTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IOptions<ConnectionStringsConfiguration> _connectionStringsConfiguration;
+
+
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger, 
+            IConfiguration configuration,
+            IOptions<ConnectionStringsConfiguration> connectionStringsConfiguration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _connectionStringsConfiguration = connectionStringsConfiguration; 
+        }
+
+        [HttpGet]
+        public string  Get()
+        {
+            string connectionString1 = _configuration.GetSection("ConnectionStrings").GetSection("sqlConnectionOracle").Value;
+            string connectionString2 = _configuration.GetValue<string>("ConnectionStrings:sqlConnectionOracle");
+            string serilogFilePath = _configuration.GetSection("SerilogFilePath").Value;
+
+            string connectionString3 = _connectionStringsConfiguration.Value.SqlConnectionOracle; 
+
+            return "Hello John"; 
+        }
+    }
+}
